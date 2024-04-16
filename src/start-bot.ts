0c83dd3a552534367cd9367c45f05b167a7517e0
Tanner S <me@tannerschermerhorn.dev>
2024-04-16 09:53:30 -0400

    Fix bot not recognizing commands

--- src/start-bot.ts
+++ src/start-bot.ts
@@ -3,7 +3,7 @@ import { Options, Partials } from 'discord.js';
 import { createRequire } from 'node:module';
 
 import { Button } from './buttons/index.js';
-import { DevCommand, HelpCommand, IPCommand, InfoCommand, TestCommand } from './commands/chat/index.js';
+import { DevCommand, HelpCommand, IPCommand, InfoCommand, TestCommand, VerifyCommand, LeaderboardCommand, PingCommand } from './commands/chat/index.js';
 import {
     ChatCommandMetadata,
     Command,
